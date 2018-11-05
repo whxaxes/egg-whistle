6126354059e4e7526e00091cb562f1ab109a16e6
wanghx <whx89768@alibaba-inc.com>
2018-11-05 18:48:13 +0800

    feat: optimize code

--- agent.js
+++ agent.js
@@ -1,4 +1,4 @@
-const { spawn, exec } = require('child_process');
+const { fork, exec } = require('child_process');
 const getPort = require('get-port');
 const constant = require('./lib/constant');
 const Whistle = require('./lib/whistle');
@@ -6,25 +6,32 @@ const whistleHost = '127.0.0.1';
 const addressConfig = {};
 
 module.exports = agent => {
-  agent.beforeStart(async () => {
-    const port = await getPort();
-    addressConfig.host = whistleHost;
-    addressConfig.port = port;
-    agent.whistle = new Whistle(agent, addressConfig);
-    runWhistle(agent, port);
+  agent.messenger.on(constant.SYNC_CONFIG, () => {
+    if (addressConfig.port) {
+      // sync config to app
+      agent.messenger.sendToApp(constant.SYNC_CONFIG, addressConfig);
+    }
   });
 
-  agent.messenger.on(constant.SYNC_CONFIG, () => {
-    if (!addressConfig.port) {
-      return;
-    }
+  agent.messenger.on('egg-ready', ({ port }) => {
+    agent.logger.info(`[egg-whistle] whistle started on http://127.0.0.1:${port}${agent.config.whistle.route}`);
+  });
 
-    // sync config to app
-    agent.messenger.sendToApp(constant.SYNC_CONFIG, addressConfig);
+  agent.beforeStart(async () => {
+    const port = await getPort();
+    await runWhistle(agent, port)
+      .then(() => {
+        addressConfig.host = whistleHost;
+        addressConfig.port = port;
+        agent.whistle = new Whistle(agent, addressConfig);
+      })
+      .catch(e => {
+        agent.logger.error('[egg-whistle] whistle start fail: ' + e.message);
+      });
   });
 };
 
-async function runWhistle(app, port) {
+function runWhistle(app, port) {
   const config = app.config.whistle;
   const argv = [ 'run', '-l', whistleHost, '-p', port ];
   const logger = app.logger;
@@ -36,7 +43,8 @@ async function runWhistle(app, port) {
     }
   });
 
-  const ps = spawn('whistle', argv);
+  const whistleFile = require.resolve('whistle/bin/whistle.js');
+  const ps = fork(whistleFile, argv, { stdio: 'pipe' });
   const exit = () => {
     if (process.platform === 'win32') {
       exec('taskkill /pid ' + ps.pid + ' /T /F');
@@ -46,14 +54,24 @@ async function runWhistle(app, port) {
   };
 
   ps.on('error', e => logger.error(e));
-  process.on('uncaughtException', exit);
-  process.on('exit', exit);
-  process.on('SIGINT', exit);
-  process.on('SIGTERM', exit);
-  process.on('SIGHUP', exit);
+  [ 'uncaughtException', 'exit', 'SIGINT', 'SIGTERM', 'SIGHUP' ].forEach(evt =>
+    process.once(evt, exit)
+  );
 
-  return {
-    ps,
-    exit,
-  };
+  return new Promise((resolve, reject) => {
+    let output = '';
+    // timeout on 5000ms
+    const tick = setTimeout(() => reject(new Error('whistle start timeout')), 5000);
+    const onData = info => {
+      output += info;
+      if (output.match(/whistle@[\d.]+ started/)) {
+        ps.stdout.removeListener('data', onData);
+        clearTimeout(tick);
+        resolve();
+      }
+    };
+
+    ps.stdout.setEncoding('utf8');
+    ps.stdout.on('data', onData);
+  });
 }
--- app.js
+++ app.js
@@ -3,11 +3,7 @@ const Whistle = require('./lib/whistle');
 
 module.exports = app => {
   app.config.coreMiddleware.unshift('whistleInterceptor');
-  app.messenger.once('egg-ready', ({ port }) => {
-    app.logger.info(
-      `[egg-whistle] whistle started on http://127.0.0.1:${port}${app.config.whistle.route}`
-    );
-
+  app.messenger.once('egg-ready', () => {
     app.messenger.sendToAgent(constant.SYNC_CONFIG);
     app.messenger.on(constant.SYNC_CONFIG, info => {
       app.whistle = new Whistle(app, info);
